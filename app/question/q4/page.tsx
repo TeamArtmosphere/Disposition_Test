8462e7a96d3f79760c3a2cfcd276e0e42645f34c
Ocknyer <kdu4045@naver.com>
2023-09-14 16:52:56 +0900

    YDYZ-51 질문 리스트 받아오기

--- app/question/q4/page.tsx
+++ app/question/q4/page.tsx
@@ -1,42 +1,59 @@
+'use client';
+
 import DefaultButton from '@/components/common/DefaultButton';
 import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../../public/imgs/logo.png';
 import ProgressBar from '@/components/Material/ProgressBar';
+import { getQuestion } from '@/api/axios-api';
 
 const Page = () => {
+  // useState type 수정 필요
+  const [questionData, setQuestionData] = useState<any>({});
+
+  useEffect(() => {
+    getQuestion(1)
+      .then(data => {
+        console.log(data);
+        setQuestionData(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   const progress = 40;
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center' }}>
       <Typography variant='h4' mb={'30px'}>
-        선호하는 장소의 유형을 선택해 주세요
+        {questionData?.question_title}
       </Typography>
-      <Box sx={{ display: 'flex', gap: '10px', alignItems: 'center', width: '100%' }}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            width: '100%',
-            gap: '20px',
-          }}
-        >
-          <Image src={logo} alt='S유형 이미지' width={70} />
-          <DefaultButton name='S' />
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            width: '100%',
-            gap: '20px',
-          }}
-        >
-          <Image src={logo} alt='P유형 이미지' width={70} />
-          <DefaultButton name='P' />
-        </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        {questionData.selections.map((question: any, idx: number) => {
+          return (
+            <Box
+              key={idx}
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%',
+                gap: '20px',
+              }}
+            >
+              <DefaultButton title={question.selection_title} size='md' />
+            </Box>
+          );
+        })}
       </Box>
       <ProgressBar progress={progress} />
     </Box>
