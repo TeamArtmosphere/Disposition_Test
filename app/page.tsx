7b2cb56446be5dfb380edb117a65ab237eca823d
Kim Jaejun <rlawowns97@gmail.com>
2023-10-06 03:03:50 +0900

    일단 메인 페이지에서 다 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import DefaultButton from '@/components/common/DefaultButton';
+// import DefaultButton from '@/components/common/DefaultButton';
 import { Box, Typography } from '@mui/material';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { FlexBoxCol } from '@/style/style';
 
@@ -15,15 +15,16 @@ export default function Home() {
 
   return (
     <Box sx={{ ...FlexBoxCol, width: '100%', height: '100%' }}>
-      <Image src={"https://artmosphere-dev-storage.s3.ap-northeast-2.amazonaws.com/logo.png"} alt='아트모스피어 로고' width={100} />
-      <Box sx={{ mt: '30px', mb: '50px', textAlign: 'center' }}>
-        <Typography variant='h3'>
-          artmosphere
-          <br />
-          유저 성향 테스트
-        </Typography>
-      </Box>
-      <DefaultButton title='테스트 시작하기' size='md' onClick={onClickStartTest} />
+        hello world
+      {/*<Image src="https://artmosphere-dev-storage.s3.ap-northeast-2.amazonaws.com/logo.png" alt='아트모스피어 로고' width={100} height={100} />*/}
+      {/*<Box sx={{ mt: '30px', mb: '50px', textAlign: 'center' }}>*/}
+      {/*  <Typography variant='h3'>*/}
+      {/*    artmosphere*/}
+      {/*    <br />*/}
+      {/*    유저 성향 테스트*/}
+      {/*  </Typography>*/}
+      {/*</Box>*/}
+      {/*<DefaultButton title='테스트 시작하기' size='md' onClick={onClickStartTest} />*/}
     </Box>
   );
 }
