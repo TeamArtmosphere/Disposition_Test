9981e9630341dd3b88697807e8bcbd7f66ae4cfe
Ocknyer <kdu4045@naver.com>
2023-10-06 17:57:57 +0900

    api test

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
