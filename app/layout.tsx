94258a63f09b8d9f2f994e8005ffa519ddf96c20
Ocknyer <kdu4045@naver.com>
2023-10-13 10:37:53 +0900

    YDYZ-51 og 이미지 절대경로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             property='og:description'
             content='ARTMOSPHERE 사용자의 PABLOS 유형을 검사합니다.'
           />
-          <meta property='og:image' content='public/imgs/ogImage.png' />
+          {/* <meta property='og:image' content='public/imgs/ogImage.png' /> */}
+          <meta
+            property='og:image'
+            content='https://plus.unsplash.com/premium_photo-1694825173178-3d2c9bbf5b5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3461&q=80'
+          />
           <meta property='og:image:width' content='1200' />
           <meta property='og:image:height' content='27' />
         </head>
